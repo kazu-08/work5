82e44369b315356ad47e96221251c583d65a7987
kazu-08 <syame.ken3@gmail.com>
2024-10-28 09:48:48 +0900

    first

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import {useState} from "react";
+
+async function fetchVerse(name){
+    const url = '${name}.jpg';
+    const response = await fetch(url);
+    return response.image();
+}
+export default function App() {
+    const [content,SetCOntent] = useState("");
+    
+    return (
+      <>
+        <header>
+          <h1>The Can Store</h1>
+        </header>
+        <div>
+          <aside>
+            <form>
+              <div>
+                <label htmlFor="category">Choose a category:</label>
+                <select id="category">
+                  <option>All</option>
+                  <option>Vegetables</option>
+                  <option>Meat</option>
+                  <option>Soup</option>
+                </select>
+              </div>
+              <div>
+                <label htmlFor="searchTerm">Enter search term:</label>
+                <input type="text" id="searchTerm" placeholder="e.g. beans" />
+              </div>
+              <div>
+                <button>Filter results</button>
+              </div>
+            </form>
+          </aside>
+          <main></main>
+        </div>
+        <footer>
+          <p>All icons found at the Noun Project:</p>
+          <ul>
+            <li>
+              Bean can icon by{" "}
+              <a href="https://thenounproject.com/yalanis/">Yazmin Alanis</a>
+            </li>
+            <li>
+              Vegetable icon by{" "}
+              <a href="https://thenounproject.com/skatakila/">Ricardo Moreira</a>
+            </li>
+            <li>
+              Soup icon by{" "}
+              <a href="https://thenounproject.com/ArtZ91/">Arthur Shlain</a>
+            </li>
+            <li>
+              Meat Chunk icon by{" "}
+              <a href="https://thenounproject.com/smashicons/">Oliviu Stoian</a>.
+            </li>
+          </ul>
+        </footer>
+      </>
+    );
+  }
